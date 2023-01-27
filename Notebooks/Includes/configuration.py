e03263fd1f5912ac245768aa006c101247209655
19harshav@gmail.com <19harshav@gmail.com>
2023-01-27 22:49:23 +0000

    commit

--- Notebooks/Includes/configuration.py
+++
@@ -1,6 +0,0 @@
-# Databricks notebook source
-raw_folder_path=f'abfss://raw@f1sa.dfs.core.windows.net'
-processed_folder_path=f'abfss://processed@f1sa.dfs.core.windows.net'
-presentation_folder_path=f'dbfs:/user/hive/warehouse/f1_presentation.db'
-storage_account_name='f1sa'
-storage_account_key='7lbW/Cb2rarnNH5il8lqOWODP2p0FSetLx7sfQkhGWs68ronFF71+YgwkZmc6BTMw+qeexPsjM1o+AStTY0cAA=='
